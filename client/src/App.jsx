f338431645a1307bd7aee355320abd19254c55bc
Raisul <raisulconnects@gmail.com>
2025-12-18 15:18:18 +0600

    Project Initialize With Vite and Tailwind CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl bg-amber-200 font-mono text-center">
+        Hello World!
+      </h1>
+    </>
+  );
+}
+
+export default App;
